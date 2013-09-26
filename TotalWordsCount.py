47b2962d0a27a6278dc6e6c280d10694d8f12516
tux3 <barrdetwix@gmail.com>
2013-09-26 21:42:53 +0200

    Add readlist.txt. Cleaner/smaller code
    - Generate a readlist.txt with each story/word count from your favorites
    per line.
    - Handle exceptions and wait for input before exiting.
    - Remove useless functions
    - Cleaned code. Now 60 lines instead of 80

--- TotalWordsCount.py
+++ TotalWordsCount.py
@@ -1,80 +1,60 @@
-import urllib.request
-import urllib.parse
-import http.cookiejar
-import http.cookies
-import sys
-
-cookie=''
 username=''
 password=''
-#proxy='http://user:password@proxy.tld:8080'
-proxy=''
-
-def init():
-    opener=0
-    cj = http.cookiejar.CookieJar()
-    auth = urllib.request.HTTPBasicAuthHandler()
-    if proxy != '':
-        proxyh = urllib.request.ProxyHandler({'http': proxy})
-        opener = urllib.request.build_opener(proxyh, auth, urllib.request.HTTPHandler, urllib.request.HTTPCookieProcessor(cj))
-    else:
-        opener = urllib.request.build_opener(auth, urllib.request.HTTPHandler, urllib.request.HTTPCookieProcessor(cj))
-    login_data = urllib.parse.urlencode({'username':username, 'password':password})
-    blogin_data = login_data.encode('ascii')
-    ret = opener.open('http://www.fimfiction.net/ajax/login.php', blogin_data)
-    urllib.request.install_opener(opener)
-    if str(ret.read()).find('0') == -1:
-        sys.exit('Login failed, check your username and password')
-    global cookie
-    cookie=ret.info()['Set-Cookie']
-    
+proxy='' # Example : proxy='http://user:password@proxy.tld:8080'
+import urllib.parse,http.cookiejar,http.cookies,sys;from urllib.request import *
 def getUrl(url):
-    req = urllib.request.Request(url)
+    req = Request(url)
     req.add_header('Cookie', cookie+'; view_mature=true')
-    conn = urllib.request.urlopen(req)
+    conn = urlopen(req)
     return str(conn.read())
-
-def findAll(string, sub, listindex=[], offset=0):
+def findAll(string, sub, offset=0):
+    listindex=[]
     i = string.find(sub, offset)
     while i >= 0:
         listindex.append(i)
         i = string.find(sub, i + 1)
     return listindex    
-    
-def addCommas(quantity):
-    return "{:,}".format(quantity)
-
-if username=='':
-    username = input("Username: ")
-if password=='':
-    password = input("Password: ")
-init()
-print('Connected to FimFiction')
-favData = getUrl('http://www.fimfiction.net/ajax/infocard_user.php?name=tux3')
-nFavs=0
-nFavPos=favData.find('=1">')
-if nFavPos>=0:
-    nFavs=int(favData[nFavPos+4:(favData[nFavPos+4:].find(' ')+nFavPos+4)])
-else:
-    sys.exit('Error finding number of favorites')
-print ('Found '+str(nFavs)+' favorites')
-nPages=-(-nFavs // 10)
-curPage=1
-nStories=0
-totalWords=0
-while curPage<=nPages:
-    print('Loading page '+str(curPage)+'/'+str(nPages)+'...')
-    url = 'http://www.fimfiction.net/index.php?view=category&tracking=1&order=updated&page='+str(curPage)
-    data=getUrl(url)
-    data=data.replace(r'\t','')
-    data=data.replace(r'\r\n','')
-    indexes=findAll(data,r'word_count"><b>')
-    pageWords=0
-    for i in indexes:
-        num=data[i+15:(data[i+15:].find('<')+i+15)]
-        pageWords+=int(num.replace(',',''))
-    indexes[:] = []
-    totalWords+=pageWords
-    curPage+=1
-print('Total words count : '+str(addCommas(totalWords)))
-input('Press enter to exit')
+def failWith(str):
+    input(str)
+    sys.exit()
+try:
+    if username=='': username=input("Username: ")
+    if password=='': password=input("Password: ")
+    if proxy!='': opener=build_opener(ProxyHandler({'http':proxy}),HTTPBasicAuthHandler(),HTTPHandler,HTTPCookieProcessor(http.cookiejar.CookieJar()))
+    else: opener=build_opener(HTTPBasicAuthHandler(),HTTPHandler,HTTPCookieProcessor(http.cookiejar.CookieJar()))
+    login_data=urllib.parse.urlencode({'username':username,'password':password}).encode('ascii')
+    ret=opener.open('http://www.fimfiction.net/ajax/login.php',login_data)
+    install_opener(opener)
+    if str(ret.read()).find('0') == -1: failWith('Login failed, check your username and password')
+    cookie=ret.info()['Set-Cookie']
+    print('Connected to FimFiction')
+    favData = getUrl('http://www.fimfiction.net/ajax/infocard_user.php?name=tux3')
+    nFavs=0;curPage=0;nStories=0;totalWords=0;nFavPos=favData.find('=1">')
+    if nFavPos>=0: nFavs=int(favData[nFavPos+4:(favData[nFavPos+4:].find(' ')+nFavPos+4)])
+    else: failWith('Error finding number of favorites')
+    print ('Found '+str(nFavs)+' favorites')
+    nPages=-(-nFavs // 10)
+    file=open('readlist.txt','w')
+    while curPage<=nPages:
+        print('Loading page '+str(curPage)+'/'+str(nPages)+'...')
+        data=getUrl('http://www.fimfiction.net/index.php?view=category&tracking=1&order=added&page='+str(curPage))
+        data=data.replace(r'\t','').replace(r'\r\n','').replace('&#039;','\'')
+        indexes=findAll(data,r'word_count"><b>')
+        indexes2=findAll(data,r'data-minimum-size="0.5">')
+        if len(indexes)!=len(indexes2):
+            j=-1
+            print('Error parsing page '+str(curPage)); file.write('Error parsing page '+str(curPage))
+        j=0;curPage+=1;
+        for i in indexes:
+            num=data[i+15:(data[i+15:].find('<')+i+15)]
+            totalWords+=int(num.replace(',',''))
+            if (j>=0):
+                ji=indexes2[j]+24
+                title=data[ji:(data[ji:].find('<')+ji)]
+                file.write(title+' '+num+'\n')
+                j+=1
+        
+    file.close()
+    print('Total words count : '+str("{:,}".format(totalWords)))
+    input('Press enter to exit')
+except: failWith('Error:'+sys.exc_info()+'\nPress enter to exit')

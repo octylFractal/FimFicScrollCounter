6431565c2848a51b09c0d0a977b9bb8014b0c3fa
Kenzie Togami <ket1999@gmail.com>
2014-10-18 15:28:22 -0700

    Make compat actually work

--- py2to3compat.py
+++ py2to3compat.py
@@ -1,25 +1,33 @@
 from __future__ import print_function
-def fix(__builtins__):
-    isdict = type(__builtins__) is dict
+def fix(builtin):
+    isdict = type(builtin) is dict
+    def moduleset(k, v):
+        setattr(builtin, k, v)
+    def moduleget(k, d=None):
+        return getattr(builtin, k, d)
     # default to module
-    delfunction = delattr
-    dbins = dir(__builtins__)
+    set_ = moduleset
+    get_ = moduleget
+    dbins = dir(builtin)
 
     if isdict:
         # dict of builtins, use it
-        dbins = __builtins__
-        delfunction = dict.pop
+        dbins = builtin
+        def dictset(k, v):
+            builtin[k] = v
+        def dictget(k, d=None):
+            return builtin.get(k, d)
+        set_ = dictset
+        get_ = dictget
 
     def has(s) :
         return s in dbins
-    def rem(s) :
-        before = eval(s)
-        # delfunction(__builtins__, s)
-        return before
+    def mv(here, there) :
+        set_(there, get_(here))
 
     if has("xrange") :
-        __builtins__.range = rem("xrange")
+        mv("xrange", "range")
     if has("raw_input") :
-        __builtins__.input = rem("raw_input")
+        mv("raw_input", "input")
 
 PYTHON_VERSION_MAJOR = __import__('sys').version_info[0]

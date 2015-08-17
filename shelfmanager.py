58ca8c3b5f147647f05d970ea3d55fe30e27f15c
Kenzie Togami <ket1999@gmail.com>
2015-08-16 20:13:21 -0700

    s/this/self/g

--- shelfmanager.py
+++ shelfmanager.py
@@ -33,69 +33,69 @@ def get_story_data(story):
     return url_cache[story]
 
 class Shelf():
-    def __init__(this, shelf_id, username='', password=''):
-        this.shelf = shelf_id
-        this.usr = username
-        this.pas = password
-        this.first_page = bs4.BeautifulSoup(get_page(this.shelf, 1, LIST_VIEW), 'lxml')
-        this.story_count = None
-        this.pages = None
-        this.stories = None
-        this.perchap_wc = None
-        this.wordcount = None
+    def __init__(self, shelf_id, username='', password=''):
+        self.shelf = shelf_id
+        self.usr = username
+        self.pas = password
+        self.first_page = bs4.BeautifulSoup(get_page(self.shelf, 1, LIST_VIEW), 'lxml')
+        self.story_count = None
+        self.pages = None
+        self.stories = None
+        self.perchap_wc = None
+        self.wordcount = None
 
-    def get_wordcount(this):
-        if this.wordcount is None:
-            print('Fetching word count for', this.shelf)
-            this.find_count()
-            this.deter_page_count()
-            this.load_stories()
-            this.wordcount = sum(get_story_data(x)['story_wc'] for x in this.stories)
-            print('Loaded word count for', this.shelf)
-        return this.wordcount
+    def get_wordcount(self):
+        if self.wordcount is None:
+            print('Fetching word count for', self.shelf)
+            self.find_count()
+            self.deter_page_count()
+            self.load_stories()
+            self.wordcount = sum(get_story_data(x)['story_wc'] for x in self.stories)
+            print('Loaded word count for', self.shelf)
+        return self.wordcount
 
-    def get_wordcount_per_chapter(this):
-        if this.perchap_wc is None:
-            print('Fetching word count per chapter for', this.shelf)
-            this.find_count()
-            this.deter_page_count()
-            this.load_stories()
-            this.perchap_wc = {}
-            for x in this.stories:
+    def get_wordcount_per_chapter(self):
+        if self.perchap_wc is None:
+            print('Fetching word count per chapter for', self.shelf)
+            self.find_count()
+            self.deter_page_count()
+            self.load_stories()
+            self.perchap_wc = {}
+            for x in self.stories:
                 data = get_story_data(x)
-                this.perchap_wc[x] = data['per_chapter_wc']
-            print('Loaded', len(this.perchap_wc), 'per-chapter word counts for', this.shelf)
-        return this.perchap_wc
+                self.perchap_wc[x] = data['per_chapter_wc']
+            print('Loaded', len(self.perchap_wc), 'per-chapter word counts for', self.shelf)
+        return self.perchap_wc
 
-    def find_count(this):
-        if this.story_count is None:
-            print('Fetching story count for', this.shelf)
-            soup = this.first_page
+    def find_count(self):
+        if self.story_count is None:
+            print('Fetching story count for', self.shelf)
+            soup = self.first_page
             count = soup(class_="search_results_count")[0]('b')[2].string
-            this.story_count = int(count)
-        return this.story_count
+            self.story_count = int(count)
+        return self.story_count
 
-    def deter_page_count(this):
-        if this.pages is None:
-            print('Determing pages for', this.shelf)
-            soup = this.first_page
+    def deter_page_count(self):
+        if self.pages is None:
+            print('Determing pages for', self.shelf)
+            soup = self.first_page
             pages = soup(class_="page_list")[0].ul('li')
             if len(pages) < 2:
                 # only one page
                 last_page = 1
             else:
                 last_page = pages[-2].string
-            this.pages = int(last_page)
-            print('Page count is', this.pages, 'for', this.shelf)
-        return this.pages
+            self.pages = int(last_page)
+            print('Page count is', self.pages, 'for', self.shelf)
+        return self.pages
 
-    def load_stories(this):
-        if this.stories is None:
-            print('Loading story urls for', this.shelf)
+    def load_stories(self):
+        if self.stories is None:
+            print('Loading story urls for', self.shelf)
             s = []
-            for page in range(this.pages):
-                print('Loading page', page, 'out of', this.pages, 'for', this.shelf)
-                soup = this.first_page if page == 0 else bs4.BeautifulSoup(get_page(this.shelf, page + 1, LIST_VIEW), 'lxml')
+            for page in range(self.pages):
+                print('Loading page', page, 'out of', self.pages, 'for', self.shelf)
+                soup = self.first_page if page == 0 else bs4.BeautifulSoup(get_page(self.shelf, page + 1, LIST_VIEW), 'lxml')
                 bold_tags = soup(class_="search_results_count")[0]('b')
                 from_ = int(bold_tags[0].string)
                 to = int(bold_tags[1].string)
@@ -104,6 +104,6 @@ class Shelf():
                 story_list = soup(class_="story-list")[0]('li')
                 for story in story_list:
                     s.append(story(class_="right")[0].h2.a['href'])
-            this.stories = tuple(s)
-            print(number_objects(len(this.stories), 'url(|s)'), 'loaded for', this.shelf)
-        return this.stories
+            self.stories = tuple(s)
+            print(number_objects(len(self.stories), 'url(|s)'), 'loaded for', self.shelf)
+        return self.stories
